f3c9db9b6926be4bf0d296b66897fe7353dc3bb6
Foma <Fomenkof7@gmail.com>
2017-07-21 00:37:29 +0200

    gulp (sass and browserSync)

---
+++ RevisoChalangeApp/gulpfile.js
@@ -0,0 +1,31 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('watch', ['browserSync'], function () {
+    gulp.watch('Views/**.cshtml', browserSync.reload);
+    gulp.watch('Scripts/**.js', browserSync.reload);
+
+    gulp.watch('Content/scss/**/*.scss', ['sass']);
+
+});
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'RevisoChalangeApp'
+        },
+    })
+})
+
+
+
+gulp.task('sass', function () {
+    return gulp.src('Content/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('Content/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['sass', 'watch']);
